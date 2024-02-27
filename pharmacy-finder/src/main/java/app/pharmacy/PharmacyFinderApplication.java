a5c8c86f8210a1e4e4896d8c148bac83203bf44f
deadWhale <rlagusrl928@gmail.com>
2024-02-27 13:00:18 +0900

    chore : project setup

---
+++ pharmacy-finder/src/main/java/app/pharmacy/PharmacyFinderApplication.java
@@ -0,0 +1,13 @@
+package app.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyFinderApplication.class, args);
+    }
+
+}
